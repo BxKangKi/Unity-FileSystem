bf88d57b865c6a07ae0e428792e9cc1108271c7d
BxKangKi <80934730+BxKangKi@users.noreply.github.com>
2024-01-19 11:10:59 +0900

    Update LogSystem.cs

--- com.bxkangki.filesystem/Runtime/LogSystem.cs
+++ com.bxkangki.filesystem/Runtime/LogSystem.cs
@@ -1,6 +1,6 @@
+using System;
 using System.Globalization;
 using UnityEngine;
-using System.IO;
 
 namespace FileSystem
 {
@@ -8,12 +8,13 @@ namespace FileSystem
     {
         public static string Path = "logs/";
         private static bool initalized = false;
+
         public static void Text(string data)
         {
 #if UNITY_EDITOR
             Debug.Log(data);
 #endif
-            var localDate = System.DateTime.Now;
+            var localDate = DateTime.Now;
             var culture = new CultureInfo("en-US");
             data = string.Format("{0} >> {1}", localDate.ToString(culture), data);
             if (initalized)
@@ -30,24 +31,23 @@ namespace FileSystem
             Text("Logging is Initalized");
         }
 
+
         private static void CreateLogFile()
         {
             FileIO.CreateDirectory(string.Format("{0}/{1}/", Application.persistentDataPath, Path));
             string p1 = string.Format("{0}/{1}/log_latest.txt", Application.persistentDataPath, Path);
-            string createDate = FileIO.GetFileLastWriteTime(p1).ToString("d");
-            string currentDate = System.DateTime.Now.Date.ToString("d");
+            DateTime modifiedDate = FileIO.GetFileLastWriteTime(p1);
+            DateTime currentDate = DateTime.Now.Date;
+
 #if UNITY_EDITOR
-            Debug.Log(createDate);
-            Debug.Log(currentDate);
+            Debug.Log(modifiedDate.ToString("d"));
+            Debug.Log(currentDate.ToString("d"));
 #endif
-            if (createDate != currentDate)
+            if (modifiedDate.ToString("d") != currentDate.ToString("d"))
             {
-                string p2 = string.Format("{0}/{1}/log_{2}.txt", Application.persistentDataPath, Path, createDate);
-                if (FileIO.CheckFile(p2))
-                {
-                    FileIO.ReplaceFile(p1, p2);
-                    FileIO.RemoveFile(p1);
-                }
+                string p2 = string.Format("{0}/{1}/log_{2}{3}{4}.txt", Application.persistentDataPath, Path, modifiedDate.Year, modifiedDate.Month, modifiedDate.Day);
+                FileIO.ReplaceFile(p1, p2);
+                FileIO.RemoveFile(p1);
             }
             FileIO.CreateFile(p1);
         }
